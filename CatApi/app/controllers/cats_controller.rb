c5dc5b1dc2051141bd93440810b149e3139feff4
nicolanzi1 <nicolanzi@gmail.com>
2020-07-29 10:33:32 +0100

    Created basic forms for index, new and show, and also improved code in the cats_controller

--- CatApi/app/controllers/cats_controller.rb
+++ CatApi/app/controllers/cats_controller.rb
@@ -17,14 +17,28 @@ class CatsController < ApplicationController
         #
         # { "cat": { "name": "Sally" } }
 
-        cat = Cat.new(name: params[:cat].permit(:name))
-        cat.admin = false
+        @cat = Cat.new(params[:cat].permit(:name))
 
-        if cat.save
-            render json: cat
+        if @cat.save
+            # cat_url == /cats/...
+            redirect_to cat_url(@cat)
         else
-            render json: cat.errors.full_messages, status: :unprocessable_entity
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
         end
+    end
+
+    # 1. GET /cats/new to fetch a form
+    # 2. User fills out form, clicks submit
+    # 3. POST /cats the data in the form
+    # 4. Create action is invoked, cat is created
+    # 5. Send client a redirect to /cats/#{id}
+    # 6. Client makes a GET request for /cats/#{id}
+    # 7. Show action for newly created cat is invoked
+
+    def new
+        # /cats/new
+        # show a form to create a new object
+        render :new
     end
 
     def update
@@ -34,10 +48,24 @@ class CatsController < ApplicationController
         # ...
     end
 
+    def edit
+        # /cats/:id/edit
+        # show a form to edit an existing object
+
+
+    end
+
     def destroy
-        # if !current_cat_user.admin
-        #     raise "error"
-        # end
-        # ...
+        # DELETE /cats/:id
+        cat = Cat.find(params[:id])
+        cat.destroy
+        redirect_to cats_url
+
+        # 1. GET /cats
+        # 2. Click delete button
+        # 3. Sends POST to /cats/123; but _method="DELETE" so rails understands
+        #    to do a destroy
+        # 4. Destroys the cat. Issues to redirect to the client
+        # 5. Client GETs /cats again
     end
 end
