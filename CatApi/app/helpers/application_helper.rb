94e817179a9d9fb78db361eb68cb7963c0d99870
nicolanzi1 <nicolanzi@gmail.com>
2020-08-06 10:31:23 +0100

    Learned about helper methods which should go inside the application helper such as link_to and button_to

--- CatApi/app/helpers/application_helper.rb
+++ CatApi/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    # <a href="<%= cat_url(cat) %>">
+    #     <%= cat.name %>
+    #     (<%= cat.description.slice(0, 20) %><%= "..." if cat.description.length > 20 %>)
+    # </a>
+
+    def cat_link(cat)
+        short_description = cat.description.slice(0, 20)
+        short_description +=  "..." if cat.description.length > 20
+
+        html = "<a href=\"#{cat_url(cat)}\">"
+        html += "#{html_escape(cat.name)} (#{h(short_description)})"
+        html += "</a>"
+
+        html.html_safe
+    end
 end
+
