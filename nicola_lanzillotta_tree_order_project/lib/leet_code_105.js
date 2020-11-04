d2c4e815b376c5a499abd29d973c3320436e7585
nicolanzi1 <nicolanzi@gmail.com>
2020-11-04 14:51:16 +0000

    Wrote a Tree Order project based on problem 105 in LeetCode for constructing a Binary Tree from preorder and inorder traversal. Also ran the code on LeetCode and passed

---
+++ nicola_lanzillotta_tree_order_project/lib/leet_code_105.js
@@ -0,0 +1,30 @@
+// View the full problem and run the test cases at:
+//  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+const { TreeNode } = require('./tree_node.js');
+
+function buildTree(preorder, inorder) {
+  if (!preorder.length && !inorder.length) return null;
+
+  let rootVal = preorder[0];
+  let root = new TreeNode(rootVal);
+
+  let midIdx = inorder.indexOf(rootVal);
+  let leftInorder = inorder.slice(0, midIdx);
+  let rightInorder = inorder.slice(midIdx + 1);
+
+  let leftPreorder = preorder.filter((val) => leftInorder.includes(val));
+  let rightPreorder = preorder.filter((val) => rightInorder.includes(val));
+
+  let leftTree = buildTree(leftPreorder, leftInorder);
+  let rightTree = buildTree(rightPreorder, rightInorder);
+
+  root.left = leftTree;
+  root.right = rightTree;
+
+  return root;
+}
+
+const preorder = [3, 9, 20, 15, 7];
+const inorder = [9, 3, 15, 20, 7];
+console.log(buildTree(preorder, inorder));
